82060b8f14296e1b928cfe4c5b1e8157147b89dc
MDSwill <m.delseppia.job@gmail.com>
2015-11-02 13:42:22 +0100

    Revert "Add Project"
    This reverts commit a7758518c6be263489afc1807f640bcba957c800.

--- willator/App/app.module.js
+++
@@ -1,9 +0,0 @@
-﻿//houses a single application-level module for your application
-
-(function () {
-    // This pattern is used with every module
-    'use strict';
-
-    angular.module('app', []);
-
-})();
