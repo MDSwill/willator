a7758518c6be263489afc1807f640bcba957c800
MDSwill <m.delseppia.job@gmail.com>
2015-11-02 13:36:33 +0100

    Add Project
    Project created in VS2015

---
+++ willator/App/app.module.js
@@ -0,0 +1,9 @@
+﻿//houses a single application-level module for your application
+
+(function () {
+    // This pattern is used with every module
+    'use strict';
+
+    angular.module('app', []);
+
+})();
