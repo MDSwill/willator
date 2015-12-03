33af37ed91b5f6ffafd7dd4f9ca1ece5e9bc5c28
MDSwill <m.delseppia.job@gmail.com>
2015-12-03 19:02:10 +0100

    New little modifies
    About section complete, portfoglio work out

---
+++ willator/assets/contact-form.php
@@ -0,0 +1,71 @@
+<?php
+	
+	$errors = array();
+
+	// Check if name has been entered
+	if (!isset($_POST['name'])) {
+		$errors['name'] = 'Per favore inserisci il tuo nome';
+	}
+	
+	// Check if email has been entered and is valid
+	if (!isset($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+		$errors['email'] = 'Per favore inserisci un indirizzo e-mail valido';
+	}
+	
+	//Check if message has been entered
+	if (!isset($_POST['massage'])) {
+		$errors['message'] = 'Per favore inserisci un messaggio';
+	}
+
+	$errorOutput = '';
+
+	if(!empty($errors)){
+
+		$errorOutput .= '<div class="alert alert-danger alert-dismissible" role="alert">';
+ 		$errorOutput .= '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+
+		$errorOutput  .= '<ul>';
+
+		foreach ($errors as $key => $value) {
+			$errorOutput .= '<li>'.$value.'</li>';
+		}
+
+		$errorOutput .= '</ul>';
+		$errorOutput .= '</div>';
+
+		echo $errorOutput;
+		die();
+	}
+
+
+
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['massage'];
+	$from = $email;
+	$to = 'marco.delseppia@gmail.com';
+	$subject = 'Infos 'bout you';
+	
+	$body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+
+	//send the email
+	$result = '';
+	if (mail ($to, $subject, $body)) {
+		$result .= '<div class="alert alert-success alert-dismissible" role="alert">';
+ 		$result .= '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+		$result .= 'Grazie, ti contatterò il prima possibile';
+		$result .= '</div>';
+
+		echo $result;
+		die();
+	}
+
+	$result = '';
+	$result .= '<div class="alert alert-danger alert-dismissible" role="alert">';
+	$result .= '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+	$result .= 'Qualcosa di strano è successo durante l'invio della mail, per favore ritenta';
+	$result .= '</div>';
+
+	echo $result;
+	
